357191b822e06b16de71ff2160f084af48ce8fd6
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-19 20:15:36 +0800

    Added Default Auth Routes, Employee Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('employee')
+    ->as('employee.')
+    ->group(function() {
+        Route::get('home', 'Home\EmployeeHomeController@index')->name('home');
+
+    	Route::namespace('Auth\Login')
+    		->group(function() {
+    			Route::get('login', 'EmployeeController@showLoginForm')->name('login');
+				Route::post('login', 'EmployeeController@login')->name('login');
+				Route::post('logout', 'EmployeeController@logout')->name('logout');
+    		});
+	});
